1682e018e03be54de13f31cc4b5fb53c2ff55d3d
Raque-Del-Castillo <raquel.sterling96@gmail.com>
2024-12-10 10:03:35 +0100

    Hola mundo

---
+++ src/main/python/com/sinensia/proyecto1/TutorialPython.py
@@ -0,0 +1,13 @@
+print("Hola Mundo")
+
+print("""Este texto 
+      se representa
+      en varias lineas""")
+#
+num_lineas = 3
+
+print(f"""Este texto
+      se representa
+      en {num_lineas} lineas
+      """)
+
