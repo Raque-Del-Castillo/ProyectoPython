2f8e9b0e633a31a619fc70efc5c88e24f624f077
Raque-Del-Castillo <raquel.sterling96@gmail.com>
2024-12-10 10:51:26 +0100

    Ejercicio identacion

---
+++ src/main/python/com/sinensia/proyecto1/identation.py
@@ -0,0 +1,11 @@
+#/usr/bin/python
+""" Probar identacion en Python. """
+A = 1
+if A == 1 :
+    print("A es 1")
+    print("Test A es 1")
+else:
+    print("A no es 1")
+    print("Test a no es 1")
+    print("Esto solo se imprime si entramos en else por la identacion")
+print("Esto siempre se imprime")
