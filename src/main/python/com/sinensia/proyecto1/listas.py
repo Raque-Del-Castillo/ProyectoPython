bff9c6548a9727123e859e7a930b6456b0478004
Raque-Del-Castillo <raquel.sterling96@gmail.com>
2024-12-11 10:29:09 +0100

    Enunciado ejercicio listas

---
+++ src/main/python/com/sinensia/proyecto1/listas.py
@@ -0,0 +1,9 @@
+#/usr/bin/python
+
+empleados = [
+    ["Pedro", ["Pyhton", "SQL"]],
+    ["Manolo", ["Java", "C++", "JavaScript"]],
+    ["Alejandro", ["HTML", "CSS", "JavaScript"]]
+]
+
+#Cambiar habilidad empleado 
