84530d6b9d38813100e73620e3234e0255f99240
Raque-Del-Castillo <raquel.sterling96@gmail.com>
2024-12-11 09:18:39 +0100

    P1 lineas

--- src/main/python/com/sinensia/proyecto1/lineas.py
+++ src/main/python/com/sinensia/proyecto1/lineas.py
@@ -2,3 +2,8 @@
 
 #Simular texto de 1000 lineas
 texto_largo = ["Linea " + str(i + 1) for i in range(1000)]
+
+for texto_largo in texto_largo:
+    if(len(texto_largo) <= 1000):
+        print(texto_largo)
+    
