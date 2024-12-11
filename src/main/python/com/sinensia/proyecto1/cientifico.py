2e28755054bb366c5ffe8424312de2cec317c1a9
Raque-Del-Castillo <raquel.sterling96@gmail.com>
2024-12-11 12:58:17 +0100

    Prefiltro real

--- src/main/python/com/sinensia/proyecto1/cientifico.py
+++ src/main/python/com/sinensia/proyecto1/cientifico.py
@@ -1,1 +1,7 @@
 #/usr/bin/python
+NITROGENO = -196
+
+lista_temperaturas = (35, 523, 56, 234, 1, -34, -53, -254, -48, -315)
+tupla_temperaturas = tuple(lista_temperaturas)
+licuado = tuple(num < NITROGENO for num in tupla_temperaturas)
+print(licuado)
